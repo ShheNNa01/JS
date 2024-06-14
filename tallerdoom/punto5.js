eeb8303b133e4577f07ec77ed1d96cddbd29081b
ShheNNa <151384675+ShheNNa01@users.noreply.github.com>
2024-06-14 12:35:46 -0500

    Js Completo
    Queda completo.

--- tallerdoom/punto5.js
+++ tallerdoom/punto5.js
@@ -1,10 +1,16 @@
 function amorchitohemosho() {
     var checkBox = document.getElementById("myCheck");
     var text = document.getElementById("text");
+    var image = document.getElementById("advaroImage");
+    var body = document.body;
 
     if (checkBox.checked) {
         text.style.display = "block";
+        image.style.display = "block";
+        body.classList.add("crazy-mode");
     } else {
         text.style.display = "none";
-    }
+        image.style.display = "none";
+        body.classList.remove("crazy-mode");
+    }
 }
